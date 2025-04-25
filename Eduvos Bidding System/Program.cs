7f990600585a80adea9dc2c14eef305722bed7e3
Rohern Dhaya <dhayarohern@gmail.com>
2025-04-25 14:18:48 +0200

    Added database connectivity

--- Eduvos Bidding System/Program.cs
+++ Eduvos Bidding System/Program.cs
@@ -1,7 +1,16 @@
+using Eduvos_Bidding_System.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(); 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+{
+    var connectionString = builder.Configuration.GetConnectionString("LoginDatabase");
+    options.UseSqlServer();
+}); //connecting to sql server
+
 
 var app = builder.Build();
 
